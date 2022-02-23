bb4e8970377ba9c94bed6d4a58631f41f5010963
wmarinic <williammarinic@gmail.com>
2022-02-23 17:41:38 +1000

    Added basic premium/watermark
    Added a premium user check and watermarked image if using free version. Basic implementation with a hard coded user/pw.

--- README.md
+++ README.md
@@ -3,13 +3,12 @@ Naive implementation of an app that users can submit text and an image URL to.
 The app returns the image with the text placed over it.
 
 
-URL request example:
-curl -X POST -d "{\"url\": \"https://upload.wikimedia.org/wikipedia/commons/3/3d/Forstarbeiten_in_%C3%96sterreich.JPG\", \"text\": \"Inpsiration Quote Here!\"}" http://localhost:3000/image
+User request example:
+curl -X POST -d "{\"url\": \"https://upload.wikimedia.org/wikipedia/commons/3/3d/Forstarbeiten_in_%C3%96sterreich.JPG\", \"text\": \"Inpsiration Quote Here!\"}" http://localhost:3000/req
 
-TODO:
-- Add error handling	
-	- invalid POST request: url or text missing
-	- unable to get image from url
-	- text too long?
-- Add watermark / premium, user db and login/logout functions
-- Add a frontend in Vue.js
+User login example:
+curl -X POST -d "{\"username\": \"test\", \"password\": \"test\"}" http://localhost:3000/user
+
+TODO:	
+- User db and login/logout functions
+- Frontend in Vue.js
--- main.go
+++ main.go
@@ -18,22 +18,58 @@ import (
 //define global vars
 var url string = ""
 var text string = ""
+var premium bool = false
+
+type request_struct struct {
+	Url  string
+	Text string
+}
+
+type user_struct struct {
+	Username string
+	Password string
+}
 
 func main() {
 	http.HandleFunc("/", homePageHandler)
-	http.HandleFunc("/image", parseUserReq)
+	http.HandleFunc("/image", imagePageHandler)
+	http.HandleFunc("/req", parseUserReq)
+	http.HandleFunc("/user", parseLogin)
 	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./images"))))
 
 	fmt.Println("Server listening on port 3000")
 	log.Panic(
 		http.ListenAndServe(":3000", nil),
 	)
-
 }
 
-type request_struct struct {
-	Url  string
-	Text string
+func homePageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<title>Inspirationifier</title>")
+	fmt.Fprintf(w, "<h1>Inspirationifier</h1>")
+	fmt.Fprintf(w, "<h2>Welcome to the Inspirationfier app!</h2>")
+	fmt.Fprintf(w, "<p>Please send a POST request with an image URL and the desired text.</p>")
+	fmt.Fprintf(w, "<p>Go to /image to see the result.</p>")
+}
+
+func parseLogin(w http.ResponseWriter, r *http.Request) {
+	//decode response
+	decoder := json.NewDecoder(r.Body)
+
+	var user user_struct
+	err := decoder.Decode(&user)
+	checkError(err)
+
+	userName := user.Username
+	passWord := user.Password
+
+	//hard coding 1 log in for now
+	if userName == "test" && passWord == "test" {
+		//premium access granted
+		premium = true
+		fmt.Println("Login successful!")
+	} else {
+		fmt.Println("Login failed, wrong username or password.")
+	}
 }
 
 func parseUserReq(w http.ResponseWriter, r *http.Request) {
@@ -55,9 +91,10 @@ func parseUserReq(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func homePageHandler(w http.ResponseWriter, r *http.Request) {
-
+func imagePageHandler(w http.ResponseWriter, r *http.Request) {
+	//check if url exists
 	if url != "" {
+		//check if text exists
 		if text != "" {
 			//get the http response from the url
 			res, err := http.Get(url)
@@ -67,34 +104,9 @@ func homePageHandler(w http.ResponseWriter, r *http.Request) {
 			data, err := ioutil.ReadAll(res.Body)
 			checkError(err)
 
-			img, _, err := image.Decode(bytes.NewReader(data))
-			checkError(err)
-
 			res.Body.Close()
 
-			// get image size
-			imgWidth := img.Bounds().Dx()
-			imgHeight := img.Bounds().Dy()
-
-			//load in a default font
-			font, err := truetype.Parse(goregular.TTF)
-			checkError(err)
-
-			face := truetype.NewFace(font, &truetype.Options{Size: 48})
-
-			// create canvas for image & drawing text
-			dc := gg.NewContext(imgWidth, imgHeight)
-			dc.DrawImage(img, 0, 0)
-			dc.SetFontFace(face)
-			dc.SetColor(color.White)
-
-			// x/y position of text
-			x := float64(imgWidth / 2)
-			y := float64(imgHeight / 2)
-			maxWidth := float64(imgWidth - 60) //maximum width text can occupy
-
-			dc.DrawStringWrapped(text, x, y, 0.5, 0.5, maxWidth, 1.5, gg.AlignCenter)
-			dc.SavePNG("images/inspirational_image.png")
+			imgOverText(data, premium)
 
 			fmt.Fprintf(w, "<title>Inspirationifier</title>")
 			fmt.Fprintf(w, "<h1>Inspirationifier</h1>")
@@ -111,8 +123,45 @@ func homePageHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+//Helper Functions
 func checkError(err error) {
 	if err != nil {
 		log.Panic(err)
 	}
+}
+
+func imgOverText(imgData []byte, premium bool) {
+	//decode from []byte to image.Image
+	img, _, err := image.Decode(bytes.NewReader(imgData))
+	checkError(err)
+
+	//get image size
+	imgWidth := img.Bounds().Dx()
+	imgHeight := img.Bounds().Dy()
+
+	//load in a default font
+	font, err := truetype.Parse(goregular.TTF)
+	checkError(err)
+
+	face := truetype.NewFace(font, &truetype.Options{Size: 48})
+
+	//create canvas for image & drawing text
+	dc := gg.NewContext(imgWidth, imgHeight)
+	dc.DrawImage(img, 0, 0)
+	dc.SetFontFace(face)
+	dc.SetColor(color.White)
+
+	//set x/y position of text
+	x := float64(imgWidth / 2)
+	y := float64(imgHeight / 2)
+	maxWidth := float64(imgWidth - 60) //maximum width text can occupy
+
+	dc.DrawStringWrapped(text, x, y, 0.5, 0.5, maxWidth, 1.5, gg.AlignCenter)
+	//check users access
+	if !premium {
+		//draw a watermark
+		dc.DrawStringAnchored("Inspirationifier: Free Version.", 325, y*2-48, 0.5, 0.5)
+	}
+	dc.SavePNG("images/inspirational_image.png")
+
 }
